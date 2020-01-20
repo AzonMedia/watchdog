2f3470a275c8b004de81bbb45d99f397297d2adc
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-20 17:49:17 +0200

    added component file

---
+++ src/Component.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\Watchdog;
+
+use Azonmedia\Components\BaseComponent;
+
+class Component extends BaseComponent
+{
+
+    protected const COMPONENT_NAME = "Watchdog package";
+    protected const COMPONENT_URL = 'https://watchdog.packages.guzaba.org/';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
+    protected const COMPONENT_VERSION = '0.0.1';
+    protected const VENDOR_NAME = 'Azonmedia';
+    protected const VENDOR_URL = 'https://azonmedia.com';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/watchdog/docs/ErrorReference/';
+}
